54ed05e03c037d9f7b7922210f166b5c3ddfc13d
Katya-pavlovich <73820035+Katya-pavlovich@users.noreply.github.com>
2020-12-10 11:34:59 +0300

    Add files via upload

---
+++ Lab5Katerina/src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileWorker fileWorker = new FileWorker();
+        fileWorker.selectionMenu();
+    }
+}
